fca21d912db3907ccc54a7f165ce432431a1de9d
wladi <wladimir2000@protonmail.com>
2023-07-02 17:46:49 -0400

    postgredb deleted

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,7 @@ WORKDIR /code
 COPY requirements.txt /code/
 
 RUN \
- apk add --no-cache postgresql-libs && \
- apk add --no-cache  gcc musl-dev postgresql-dev && \
+ apk add --no-cache  gcc musl-dev && \
  python3 -m pip install -r requirements.txt --no-cache-dir
 
 RUN apk add bash
--- portfolio_page/settings.py
+++ portfolio_page/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,19 +77,6 @@ WSGI_APPLICATION = 'portfolio_page.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 #the database it´s created in the docker-compose.yml file
-import os
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': 5432,
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
