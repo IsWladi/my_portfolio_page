ba4e5ff883515739173c39ddee6bdcb082a43e92
wladi <wladimir2000@protonmail.com>
2023-07-23 12:54:09 -0400

    Added event to Stack component and languageSelector border radius.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
+// Import components
+import "./components/languageSelector.js";
+import "./components/stack.js";
+import "./components/project.js";
+import "./components/upButton.js";
+// Import modules and functions
 import { setAllMessages } from "./modules/languages/setMessages.js";
 import { manageStackComponents } from "./components/componentsManagement/stackManagement.js";
 
 // Set translated messages to the DOM
-  setAllMessages();
+setAllMessages();
 
 // Give funcitonality to the components
-  setTimeout(function () {
-    manageStackComponents();
-}, 600); // Await for for the component being inserted
-
+manageStackComponents();
