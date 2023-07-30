19e8b288f641a68f5b80c15f8624480e2af2a2b9
wladi <wladimir2000@protonmail.com>
2023-07-29 23:40:39 -0400

    Navbar event listener.

--- js/index.js
+++ js/index.js
@@ -5,11 +5,14 @@ import "./components/project.js";
 import "./components/upButton.js";
 // Import modules and functions
 import { setAllMessages } from "./modules/languages/setMessages.js";
+import { navbarCollapseButton } from "./modules/navbar/navbarCollapseButton.js";
 import { manageLanguageComponents } from "./components/componentsManagement/languageSelectorManagement.js";
 import { manageStackComponents } from "./components/componentsManagement/stackManagement.js";
 
 // Set translated messages to the DOM
 setAllMessages();
+// Event listener to the navbar for changing the navbar´s icon when the navbar is collapsed
+navbarCollapseButton();
 
 // Give funcitonality to the components
 manageLanguageComponents();
